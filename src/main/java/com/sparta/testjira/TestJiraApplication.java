0ad248502473a2e3d732fff290bb15ecad6817d5
dami0806 <dami0804@naver.com>
2024-07-10 20:27:44 +0900

    git init

---
+++ src/main/java/com/sparta/testjira/TestJiraApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.testjira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/testjira/TestJiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.testjira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
